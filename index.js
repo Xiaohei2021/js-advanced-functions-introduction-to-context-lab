bd2ccab3a97fd9cf6d9bd7adcaeae7f532004291
Leon <hualiang.tian@gmail.com>
2021-09-26 20:27:20 -0400

    commit tree c6c7d8de6e883960608f9512df9a6e4d9af55f22

--- index.js
+++ index.js
@@ -1,1 +1,92 @@
 // Your code here
+
+
+
+function createEmployeeRecord(row)
+{
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arrays)
+{
+   return arrays.map(createEmployeeRecord)
+}   
+
+function createTimeInEvent(emp, punchCard)
+{
+    let [date, hour] = punchCard.split(" ")
+
+    emp.timeInEvents.push
+    (
+        {
+            type: "TimeIn",
+            hour: parseInt(hour),
+            date
+        }
+    )
+    return  emp
+}
+
+function createTimeOutEvent(emp, punchCard)
+{
+    let [date, hour] = punchCard.split(" ")
+
+    emp.timeOutEvents.push
+    (
+        {
+            type: "TimeOut",
+            hour: parseInt(hour),
+            date
+        }
+    )
+    return  emp
+} 
+
+function hoursWorkedOnDate(emp, punchCard)
+{
+    const timesIn = emp.timeInEvents.find(e => e.date === punchCard);
+    const timesOut = emp.timeOutEvents.find(e => e.date === punchCard);
+
+    return(timesOut.hour - timesIn.hour)/100
+}
+
+function wagesEarnedOnDate(emp, punchCard)
+{
+ const payRate = emp.payPerHour;
+ const workTime = hoursWorkedOnDate(emp, punchCard);
+ return (payRate * workTime);   
+}
+
+function allWagesFor(emp)
+{
+    let daysWorked = emp.timeInEvents.map(day => day.date)
+    let payCheck = daysWorked.reduce(function(accumulator,day)
+    {
+        return accumulator + wagesEarnedOnDate(emp, day)
+    },0)
+
+    return payCheck;
+}
+
+
+function findEmployeeByFirstName(srcArray, firstName)
+{
+    return srcArray.find(element => element.firstName === firstName)
+
+}
+
+
+function calculatePayroll(array)
+{
+   return array.reduce(function(accumulator, day)
+   {
+        return accumulator + allWagesFor(day)
+   },0) 
+}
